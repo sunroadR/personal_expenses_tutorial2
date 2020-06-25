e58f4bec6f98f4f5fdfece0691e0280d64404be1
solveigraddum <solveigraddum@gmail.com>
2020-06-25 12:32:57 +0200

    transaction, widgets new_transaction, transaction_list , user_transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './transaction.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,20 +17,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1',
-        title: 'New Shoes',
-        amount: 69.99,
-        date: DateTime.now()
-    ),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.53,
-        date: DateTime.now()
-    )
-  ];
+
+
+//  String titleInput;
+//  String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -51,87 +42,16 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            elevation: 5,
 
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: ('Title')
-                  ),
-
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: ('Amount')
-                  ),
-                ),
-                FlatButton(
-                  child: Text('Add Transaction'),
-                  textColor: Colors.purple,
-                  onPressed: (){},
-
-                )
               ],
           ),
-            ),),
-
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 1,
-
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple
-                          ),
-                        ),
-
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-
-                        children: <Widget>[
 
 
-                          Text(tx.title,
-                          style: TextStyle(
-                              fontSize: 16,
-                               fontWeight: FontWeight.bold),
-                               ),
-                           Text(
-                                DateFormat('dd/MM/yyyy').format(tx.date),
-                          style: TextStyle(fontSize: 18,
-                          color: Colors.black),),
-                        ],
-                      )
-                    ],
-                  ));
-            }).toList(),),
 
 
-        ],
-      ),
+
+
+
     );
     // TODO: implement build
     throw UnimplementedError();
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,75 @@
+
+
+ import 'package:flutter/material.dart';
+ import 'package:intl/intl.dart';
+
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+
+
+   List<Transaction> _userTransactions;
+
+  TransactionList (this._userTransactions);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: _userTransactions.map((tx) {
+        return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 1,
+
+                      )
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                    ),
+                  ),
+
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: <Widget>[
+
+
+                    Text(tx.title,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat('dd/MM/yyyy').format(tx.date),
+                      style: TextStyle(fontSize: 18,
+                          color: Colors.black),),
+                  ],
+                )
+              ],
+            ));
+      }).toList()
+
+    );
+
+
+
+  }
+
+}
