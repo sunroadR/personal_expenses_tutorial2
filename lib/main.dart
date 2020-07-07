febf8519eb014bf56bf6bb612374b9192e8854de
solveigraddum <solveigraddum@gmail.com>
2020-07-07 19:59:13 +0200

    images

--- lib/main.dart
+++ lib/main.dart
@@ -56,13 +56,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+  /**  Transaction(
+        id: 't1',
+    title: 'New Shoes',
+    amount: 69.99, date:
+    DateTime.now()),
     Transaction(
         id: 't2',
         title: 'Weekly Groceries',
         amount: 16.53,
-        date: DateTime.now())
+        date: DateTime.now())*/
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
