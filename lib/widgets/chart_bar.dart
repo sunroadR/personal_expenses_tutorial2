b7e49fa22448f9c20b4c224175b15924b05f9436
solveigraddum <solveigraddum@gmail.com>
2020-07-19 16:32:11 +0200

    ChartBar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,8 +13,10 @@ class ChartBar extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return Column(
+
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(),
+        FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
         SizedBox(height: 4 ),
         Container(
           height: 60,
